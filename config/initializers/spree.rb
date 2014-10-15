0d1143d5f9c0e6bcc468aa9ff9b2a1304ea7a957
Paul Slusar <paul@knowhowexperts.com>
2014-10-15 13:30:55 -0700

    Initial commit

---
+++ config/initializers/spree.rb
@@ -0,0 +1,17 @@
+# Configure Spree Preferences
+#
+# Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
+#       If you would like users to be able to update a setting with the Admin it should NOT be set here.
+#
+# In order to initialize a setting do:
+# config.setting_name = 'new value'
+Spree.config do |config|
+  # Example:
+  # Uncomment to stop tracking inventory levels in the application
+  # config.track_inventory_levels = false
+  config.admin_interface_logo = 'logo/eduqella_admin.png'
+  config.logo = 'logo/eduqella.png'
+
+end
+
+Spree.user_class = "Spree::User"
